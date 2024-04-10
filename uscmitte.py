91674803f6c08b616eb05d4529df65004c6adfcf
pmahan00 <pradyumna.mahanta@gmail.com>
2024-04-10 14:06:37 +0200

    Add files via upload
    updated script tp traverse through 14 days

--- uscmitte.py
+++ uscmitte.py
@@ -10,7 +10,7 @@ email = ""
 password = ""
 # Calculate the date 5 days from today
 today = datetime.now()
-target_date = today + timedelta(days=4)
+target_date = today + timedelta(days=13)
 # Format the date to match the format used in your page.get_by_text() call
 # Use %a for abbreviated weekday name, remove the last letter, and remove leading zeros by converting day to an integer
 formatted_date = f"{target_date.strftime('%a')[:-1]} {int(target_date.strftime('%d'))}"
@@ -19,7 +19,7 @@ async def run(playwright: Playwright):
     chromium = playwright.firefox # or "firefox" or "webkit".
     browser = await chromium.launch(headless = False)
     page = await browser.new_page()
-    await page.goto("https://urbansportsclub.com/en/venues/playground-berlin") #Testing playground berlin booking , change this to community sports 
+    await page.goto("https://urbansportsclub.com/en/venues/community-sports-beachmitte") #Testing playground berlin booking , change this to community sports 
     await page.get_by_role("button", name="Accept all cookies").click()
     await page.get_by_role("link", name="Log in").click()
     await page.get_by_placeholder("Email *").click()
@@ -29,14 +29,17 @@ async def run(playwright: Playwright):
     await page.get_by_role("insertion").click()
     await page.get_by_role("button", name="Login").click()
     # courses 
-    await page.get_by_text("Free Trainings", exact=True).click()
+    #await page.get_by_text("Free Trainings", exact=True).click()
     print("courses")
+    await page.get_by_role("button", name="").click()
     await page.get_by_text(formatted_date).click()
     print(formatted_date)
+    await page.get_by_text("Community Sports – Beachvolleyball Beach Volleyball").click()
+
     #await page.get_by_text("19:00 — 21:00").click()----> this doesnt give me anything but time
     #await page.get_by_role("link", name="Beachvolleyball Training A2/A3 (-50%)").nth(1).click()
     #await page.get_by_role("link", name="Continue").second.click()---> first continue button (this should be the case for USC)
-    await page.get_by_role("link", name="Continue").nth(2).click() #---> second continuebutton
+    # await page.get_by_role("link", name="Continue").nth(2).click() #---> second continuebutton
         # Check if "Instant book" button is present
     try:
         await page.wait_for_selector('button:has-text("Instant book")', state='visible', timeout=5000)
